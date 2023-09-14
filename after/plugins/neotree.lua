eb4b17cd0aa022e1ba7eedd347aaaa3b0769e048
ldufour <119424972+lpdbond15@users.noreply.github.com>
2023-09-14 08:50:57 -0400

    small push

--- after/plugins/neotree.lua
+++
@@ -1,1 +0,0 @@
-vim.keymap.set('n', '<C-f>', "<cmd>Neotree toggle<cr>")
--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -3,10 +3,10 @@ return {
 vim.keymap.set("n", "<leader>ha", require('harpoon.mark').add_file),
 vim.keymap.set("n", "<leader>hv", require('harpoon.ui').toggle_quick_menu),
 
-vim.keymap.set("n", "1", function() require('harpoon.ui').nav_file(1) end),
-vim.keymap.set("n", "2", function() require('harpoon.ui').nav_file(2) end),
-vim.keymap.set("n", "3", function() require('harpoon.ui').nav_file(3) end),
-vim.keymap.set("n", "4", function() require('harpoon.ui').nav_file(4) end),
+vim.keymap.set("n", "<leader>1", function() require('harpoon.ui').nav_file(1) end),
+vim.keymap.set("n", "<leader>2", function() require('harpoon.ui').nav_file(2) end),
+vim.keymap.set("n", "<leader>3", function() require('harpoon.ui').nav_file(3) end),
+vim.keymap.set("n", "<leader>4", function() require('harpoon.ui').nav_file(4) end),
 vim.keymap.set('n', '<C-f>', "<cmd>Neotree toggle<cr>"),
 
 
